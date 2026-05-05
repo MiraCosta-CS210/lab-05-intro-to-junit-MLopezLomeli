a6513645ec0e167eaa69486d7b0c4ee4e9d83d2e
Marisol Lopez Lomeli <lopezlomelimarisol@gmail.com>
2026-05-05 15:33:01 -0700

    Add unit tests for sumIsEven method

--- tests/SumNumbersTest.java
+++ tests/SumNumbersTest.java
@@ -1,2 +1,35 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SumNumbersTest {
+
+    @org.junit.jupiter.api.Test
+    void EvenTest1(){
+        SumNumbers n1 = new SumNumbers();
+        int sumEven = n1.sumIsEven(1,1);
+        assertEquals(1, sumEven);
+    }
+
+    @org.junit.jupiter.api.Test
+    void EvenTest2(){
+        SumNumbers n1 = new SumNumbers();
+        int sumEven = n1.sumIsEven(1,2);
+        assertEquals(0, sumEven);
+    }
+
+    @org.junit.jupiter.api.Test
+    void EvenTest3(){
+        SumNumbers n1 = new SumNumbers();
+        int sumEven = n1.sumIsEven(2,2);
+        assertEquals(1, sumEven);
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void EvenTest4(){
+        SumNumbers n1 = new SumNumbers();
+        int sumEven = n1.sumIsEven(3,2);
+        assertEquals(0, sumEven);
+    }
+
+
 }
