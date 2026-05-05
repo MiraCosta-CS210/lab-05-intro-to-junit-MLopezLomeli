ff79e2e00713690ed4da47e36d781370dc1826a1
Marisol Lopez Lomeli <lopezlomelimarisol@gmail.com>
2026-05-05 15:33:31 -0700

    Add unit tests for TipCalculator class

--- tests/TipCalculatorTest.java
+++ tests/TipCalculatorTest.java
@@ -1,2 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TipCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void TipTest1(){
+        TipCalculator tip1 = new TipCalculator();
+        double tipAmount = tip1.calculateTip(100,20);
+        assertEquals(20.00, tipAmount);
+    }
+
+    @org.junit.jupiter.api.Test
+    void TipTest2(){
+        TipCalculator tip1 = new TipCalculator();
+        double tipAmount = tip1.calculateTip(27.05,18);
+        assertEquals(4.87, tipAmount);
+    }
+
+    @org.junit.jupiter.api.Test
+    void TipTest3(){
+        TipCalculator tip1 = new TipCalculator();
+        double tipAmount = tip1.calculateTip(1128.02,37);
+        assertEquals(417.37, tipAmount);
+    }
+
+
 }
